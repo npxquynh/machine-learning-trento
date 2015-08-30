cdea8c7997eacea729970ba005fc75d30162fdfe
Quynh Nguyen <quynh.xq@gmail.com>
2015-08-31 01:32:52 +0200

    Split dataset into 100 samples for training and 100 samples for test

--- project1/split_dataset.py
+++ project1/split_dataset.py
@@ -12,9 +12,21 @@ data = numpy.genfromtxt('./heart.dat', dtype=None, delimiter=',', skip_header=1)
 y_data = data[:, 1]
 X_data = data[:, 2:]
 
+# randomly sample 200 samples from heart.dat
+indices = set()
+numpy.random.seed(1234)
+while len(indices) != 200:
+    indices.add(numpy.random.randint(0, len(y_data)))
+
+indices = list(indices)
+y_data = y_data[indices]
+X_data = X_data[indices]
+data = data[indices]
+
 kfold = sklearn.cross_validation.StratifiedKFold(y_data, n_folds=2, shuffle=True, random_state=1234)
 
 for (i, (train_index, test_index)) in enumerate(kfold):
+    debugger()
     train_data = data[train_index]
     test_data = data[test_index]
 
